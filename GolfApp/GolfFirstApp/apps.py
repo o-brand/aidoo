82ae74908817bacb2bc7b8b980cd699c2e41f13b
o-brand <92092373+o-brand@users.noreply.github.com>
2022-10-13 12:12:30 +0100

    Delete GolfApp directory

--- GolfApp/GolfFirstApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GolffirstappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GolfFirstApp'
--- GolfApp/GolfFirstApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from GolfFirstApp import views
-
-urlpatterns = [
-    path('GolfFirstApp/', views.GolfFirstApp, name='GolfFirstApp'),
-]
