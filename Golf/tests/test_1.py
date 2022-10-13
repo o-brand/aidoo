c7c8886f2194ab08a1f205931c6511875ab2ee42
Rosta Gábor <rostagabor13@gmail.com>
2022-10-13 12:32:03 +0100

    Edit test

--- Golf/tests/test_1.py
+++ Golf/tests/test_1.py
@@ -1,5 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
 class LoginTestCase(TestCase):
-    pass
+
+    def setUp(self):
+        self.credentials = {
+            'username': 'golf',
+            'password': 'TeamGolf2022'
+        }
+        User.objects.create_user(**self.credentials)
+
+    def test_login(self):
+        # send login data
+        response = self.client.post('/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
