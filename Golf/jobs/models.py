6e14f72ce99bb6bc549e0d8c717fccb10559e6f4
Rosta Gábor <rostagabor13@gmail.com>
2022-10-17 16:07:00 +0100

    Added foreign key for JobPosting

--- Golf/jobs/models.py
+++ Golf/jobs/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 
 # Create your models here.
 class JobPosting(models.Model):
     job_id = models.BigAutoField(primary_key=True)
     location = models.CharField(max_length=7)
-    # poster_id = models.ForeignKey('Poster ID', on_delete=models.CASCADE,)
+    poster_id = models.ForeignKey(User, on_delete=models.CASCADE) # Should we use something else on delete?
     job_title = models.CharField(max_length=50)
     job_short_description = models.CharField(max_length=50)
     job_description = models.CharField(max_length=1000)
