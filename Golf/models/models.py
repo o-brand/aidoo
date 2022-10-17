1012b490ed5ada1d84f1a21dd4412261da9a462e
Gábor Rosta <32847735+gaborrosta@users.noreply.github.com>
2022-10-17 15:34:58 +0100

    Update models.py

--- Golf/models/models.py
+++ Golf/models/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class JobPosting(models.Model):
     job_id = models.BigAutoField(primary_key=True)
-    poster_id = models.ForeignKey('Poster ID', on_delete=models.CASCADE,)
+    location = models.CharField(max_length=7)
+    # poster_id = models.ForeignKey('Poster ID', on_delete=models.CASCADE,)
     job_title = models.CharField(max_length=50)
-    job_description = models.CharField(max_length=300)
+    job_short_description = models.CharField(max_length=50)
+    job_description = models.CharField(max_length=1000)
     posting_time = models.DateTimeField()
-    points = models.IntegerField() #can we make sure it's positive?
-    category = models.CharField(max_length=50)
+    points = models.IntegerField(validators=[MinValueValidator(1)])
     assigned = models.BooleanField()
     completed = models.BooleanField()
