7e2fcffccbabc412443b0fdf16be3fa8af81e841
Roman Berger <romanberger001@gmail.com>
2022-10-17 16:40:52 +0100

    show jobs

--- Golf/jobs/views.py
+++ Golf/jobs/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import JobPosting
+
+def index(request):
+    print("this is where the index goes")
+
+def individualPost(request):
+    recent_job = JobPosting.objects.get(id_exact = 1)
+    return HttpResponse(recent_job.job_title + ':' + recent_job.job_description)
+
 
 # Create your views here.
