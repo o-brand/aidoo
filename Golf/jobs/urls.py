4c268379fc909fc8415fbfc3e53a2a330f675c4f
Rosta Gábor <rostagabor13@gmail.com>
2022-10-18 11:17:12 +0100

    Jobs displayed + Added test data

--- Golf/jobs/urls.py
+++ Golf/jobs/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # Be aware that the url already includes "jobs/"
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'), # Basic home
+    path('', views.JobsView.as_view(), name='home'), # Basic home with the jobs list
     path('jobs/',views.individualPost, name='jobs'),
     path('post/', TemplateView.as_view(template_name='postjob.html'), name='post'), # Posting a job
 ]
--- Golf/jobs/views.py
+++ Golf/jobs/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import JobPosting
+from django.views.generic import ListView
 
 def individualPost(request):
     recent_job = JobPosting.objects.get(pk=1)
     return HttpResponse(recent_job.job_title + ':' + recent_job.job_description)
+
+class JobsView(ListView):
+   model               = JobPosting
+   template_name       = 'home.html'
+   context_object_name = 'jobs'
+
+   def get_queryset(self):
+        return JobPosting.objects.all()
