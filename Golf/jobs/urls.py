fff05dadf27e31983fba6760bee71cf05016794d
Rosta Gábor <rostagabor13@gmail.com>
2022-10-19 15:04:14 +0100

    Added details about jobs

--- Golf/jobs/urls.py
+++ Golf/jobs/urls.py
@@ -5,6 +5,6 @@ from . import views
 # Be aware that the url already includes "jobs/"
 urlpatterns = [
     path('', views.JobsView.as_view(), name='home'), # Basic home with the jobs list
-    path('jobs/',views.individualPost, name='jobs'),
     path('post/', TemplateView.as_view(template_name='postjob.html'), name='post'), # Posting a job
+    path('<int:job_id>/', views.detail, name='jobdetails'), # Details of a job
 ]
--- Golf/jobs/views.py
+++ Golf/jobs/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import JobPosting
 from django.views.generic import ListView
+from django.template import loader
 
-def individualPost(request):
-    recent_job = JobPosting.objects.get(pk=1)
-    return HttpResponse(recent_job.job_title + ':' + recent_job.job_description)
+def detail(request, job_id):
+    template = loader.get_template('jobdetails.html')
+    context = {
+        'job': JobPosting.objects.get(pk=job_id),
+    }
+    return HttpResponse(template.render(context, request))
 
 class JobsView(ListView):
    model               = JobPosting
