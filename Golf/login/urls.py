359f31208ae24778b7b776f9f537398e263022e3
Kieronr142 <u09kr18@abdn.ac.uk>
2022-10-20 15:14:22 +0100

    Sign up Implementation

--- Golf/login/urls.py
+++ Golf/login/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from .views import SignUpView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='welcome.html'), name='welcome'), # Welcome page for the whole project
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), # Login
     path('logout/', auth_views.LogoutView.as_view(), name='logout'), # Log out
+
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
