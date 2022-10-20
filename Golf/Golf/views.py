6ae011687bfdc07001089d9f418290151d3dea3b
o-brand <92092373+o-brand@users.noreply.github.com>
2022-10-20 02:15:27 +0100

    Update views.py

--- Golf/Golf/views.py
+++ Golf/Golf/views.py
@@ -10,7 +10,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo("golf/Golf/Golf") 
+        repo = git.Repo("golf") 
         origin = repo.remotes.origin
 
         origin.pull()
