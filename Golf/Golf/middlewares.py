062da73b0e3e0b2e9900a2fdef383fa54831fa65
Rosta Gábor <rostagabor13@gmail.com>
2022-10-26 14:00:40 +0100

    Updated middleware

--- Golf/Golf/middlewares.py
+++ Golf/Golf/middlewares.py
@@ -13,7 +13,7 @@ class LoginRequiredMiddleware():
         if not request.user.is_authenticated:
             path = request.path_info.lstrip('/')
 
-            if path[-1] == '/':
+            if len(path) > 0 and path[-1] == '/':
                 path = path[0:-1]
 
             if path in ENABLED_URLS or path.startswith("admin") or path.startswith("password_reset"):
