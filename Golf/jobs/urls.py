7bebe21c74f3859e6411fdcd1b35a03e8126a25a
anet.k <u06ak20@abdn.ac.uk>
2022-10-27 15:59:58 +0100

    working submit forms function

--- Golf/jobs/urls.py
+++ Golf/jobs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
+from django.views.generic.base import View
+
 
 # Be aware that the url already includes "jobs/"
 urlpatterns = [
--- Golf/jobs/views.py
+++ Golf/jobs/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import JobPosting
 from django.views.generic import ListView
+from django.views import View
 from django.template import loader
 from .forms import JobForm
 
@@ -10,6 +11,7 @@ def individualPost(request):
     template = loader.get_template('postjob.html')
     form = JobForm(request.POST)
     context= {'form':form}
+    FormView.return_submit(request)
     print("smth")
     return HttpResponse(template.render(context, request))
 
@@ -33,3 +35,29 @@ class JobsView(ListView):
 
    def get_queryset(self):
         return JobPosting.objects.all()
+
+def testcall(request):
+    print("HI", request.POST['text'])
+    time.sleep(3) # Sleep...
+    return HttpResponse("ok")
+
+class FormView(View):
+    form_class = JobForm
+    template_name = "postjob.html"
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def return_submit(request):
+        form = JobForm(request.POST)
+        if request.method == "POST":            
+            print("here")
+        
+        #this if contained within the first if
+            if form.is_valid():
+                form.save(commit=False)
+                print("im here")
+                #return redirect("views: return_submit")
+        form = JobForm()
+        return render(request, FormView.template_name, {'form': form})
+        
