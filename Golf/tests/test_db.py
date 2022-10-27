fcee39621ed76d3338f714f426a4d44c9069bc61
Roman Berger <romanberger001@gmail.com>
2022-10-27 12:53:39 +0100

    User db tests work now

---
+++ Golf/tests/test_db.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from jobs.models import JobPosting
+from faker import Faker
+
+class UserTableTestCase(TestCase):
+
+    def setUp(self):
+        fake = Faker()
+        # write 10 users into the table
+        for i in range(1):
+            credentials = dict()
+            credentials['username'] = lambda: fake.unique.pronounceable_unique_id(length=30)
+            credentials['password'] = 'a'
+            credentials['last_name'] = lambda: fake.last_name()
+            credentials['first_name'] = lambda: fake.first_name()
+            User.objects.create_user(**credentials)
+
+    def test_retrieve_user(self):
+
+        u = User.objects.get(pk=1)
+
+        self.assertEqual(u.id, 1)
+
+    def test_create_user(self):
+        len1 = len(User.objects.all())
+
+        credentials = dict()
+        credentials['username'] = lambda: fake.unique.pronounceable_unique_id(length=30)
+        credentials['password'] = 'a'
+        credentials['last_name'] = lambda: fake.last_name()
+        credentials['first_name'] = lambda: fake.first_name()
+        User.objects.create_user(**credentials)
+
+        len2 = len(User.objects.all())
+
+        self.assertEqual(len1+1, len2)
+
+    def test_delete_user(self):
+        u = User.objects.get(pk=1)
+        len1 = len(User.objects.all())
+
+        u.delete()
+
+        len2 = len(User.objects.all())
+
+        self.assertEqual(len1-1, len2)
+
+class JobTableTestCase(TestCase):
+
+    def setUp(self):
+        fake = Faker()
+        #write 10 users into users table
+        for i in range(10):
+            #User.objects.create_user()
+            pass
+        #write 10 jobs into the job_postings table
+        for i in range(10):
+            #JobPosting.objects.create_jobPosting()
+            pass
+        pass
+
+    def test_retrieve_job(self):
+        pass
+
+    def test_create_job(self):
+        pass
+
+    def test_delete_jon(self):
+        pass
