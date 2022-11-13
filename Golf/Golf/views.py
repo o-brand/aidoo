70d13504e4a4b1c1a906318ad5979290d3cb8f76
o-brand <o.brand.20@abdn.ac.uk>
2022-11-13 20:23:39 +0000

    Webhook with subprocess 2

--- Golf/Golf/views.py
+++ Golf/Golf/views.py
@@ -11,7 +11,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        subprocess.check_call("cd  /home/teamgolf/golf; git pull", shell=True)
+        subprocess.check_call("cd  /home/teamgolf/golf; git fetch --all; git reset --hard origin/main; ", shell=True)
         return HttpResponse("Updated code on PythonAnywhere")
     else:
         return HttpResponse("Couldn't update the code on PythonAnywhere")
