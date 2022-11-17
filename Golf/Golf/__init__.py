9ad2121963121ba753d7cfeb05e1ecf9cdf53f7f
Rosta Gábor <rostagabor13@gmail.com>
2022-11-17 21:41:20 +0000

    Golf folder I.

--- Golf/Golf/__init__.py
+++ Golf/Golf/__init__.py
@@ -1,6 +1,9 @@
-# Alex Aarons - alex2022
-# Beatrice Bailey - beatrice2022
-# Claire Collins - claire2022
-# Duncan Delgado - duncan2022
-# Erica Earl - erica2022
-# Flora Forbes - flora2022
+"""
+Users:
+- Alex Aarons: alex2022
+- Beatrice Bailey: beatrice2022
+- Claire Collins: claire2022
+- Duncan Delgado: duncan2022
+- Erica Earl: erica2022
+- Flora Forbes: flora2022
+"""
--- Golf/Golf/middlewares.py
+++ Golf/Golf/middlewares.py
@@ -1,24 +1,43 @@
 from django.http import HttpResponseRedirect
 
-ENABLED_URLS = ("", "login", "signup", "logout", "privacy", "update_server","account_activation_email")
 
-class LoginRequiredMiddleware():
+# These are the urls which are available (even) if the user is NOT
+# authenticated.
+ENABLED_URLS = ("", "login", "signup", "logout", "privacy",
+    "account_activation_email")
+
+
+class LoginRequiredMiddleware:
+    """It is used to check if the user is authenticated."""
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        """This function is called when communication starts with Django."""
         response = self.get_response(request)
 
+        # Check if the user is authenticated
         if not request.user.is_authenticated:
-            path = request.path_info.lstrip('/')
+            path = request.path_info.lstrip("/")
 
-            if len(path) > 0 and path[-1] == '/':
+            # Is it the welcome page?
+            if len(path) > 0 and path[-1] == "/":
                 path = path[0:-1]
 
-            if path in ENABLED_URLS or path.startswith("admin") or path.startswith("password_reset") or path.startswith("activate") or path.startswith("confirm_email"):
+            # Is the url enabled or starts with enabled part?
+            if (
+                path in ENABLED_URLS
+                or path.startswith("admin")
+                or path.startswith("password_reset")
+                or path.startswith("activate")
+                or path.startswith("confirm_email")
+            ):
                 return response
 
+            # Redirect to the login page (but after login the user is
+            # redirected to the requested page)
             return HttpResponseRedirect("/login/?next=/" + path)
 
+        # The user can see this page
         return response
