036ab0cced04785a2d0f1ce84b0ee2068bea941c
anet.k <u06ak20@abdn.ac.uk>
2022-11-24 10:23:18 +0000

    clean up of userprofile, and templates/userpofile

--- Golf/userprofile/apps.py
+++ Golf/userprofile/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
+
 class UserprofileConfig(AppConfig):
+    """This is the config class for the UserProfile app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userprofile'
--- Golf/userprofile/urls.py
+++ Golf/userprofile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 # Be aware that the url already includes "profile/"
 urlpatterns = [
     path('<int:user_id>/', views.userdetail, name='userdetail'), # Details of a user
     path('me/', views.me, name='me') # My profile
-    ]
+]
