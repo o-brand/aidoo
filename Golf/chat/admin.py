6602228218ec3dad5288e07e436c434d79548f21
Rosta Gábor <rostagabor13@gmail.com>
2023-02-09 17:56:03 +0000

    Created Room model + DB update

---
+++ Golf/chat/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Room
+
+
+# Enable editing these models via /admin/.
+admin.site.register(Room)
---
+++ Golf/chat/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+
+# Get actual user model.
+User = get_user_model()
+
+
+class Room(models.Model):
+    """This model is used to represent a room where two user can chat."""
+
+    # Primary key
+    room_id = models.BigAutoField(primary_key=True)
+
+    # Foreign key to the User model (user who created the room)
+    user_1 = models.ForeignKey(User, related_name= "inviter", on_delete=models.CASCADE)
+
+    # Foreign key to the User model (the other user)
+    user_2 = models.ForeignKey(User, related_name= "invitee", on_delete=models.CASCADE)
+
+    # The time of creation (it has default value)
+    creation_time = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        """This class creates a contraint for the model."""
+
+        # Prevent a user to have two rooms with somebody
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user_1", "user_2"], name="user_user_combo"
+            )
+        ]
