c71869ac67ecd90643a9d37d80bc5a7e7b8728f6
Roman Berger <romanberger001@gmail.com>
2023-02-14 14:55:43 +0000

    basic tests

--- Golf/superadmin/tests.py
+++ Golf/superadmin/tests.py
@@ -1,1 +1,16 @@
 from django.test import TestCase
+
+class TestCaseReporting(TestCase):
+
+    def setUp(self):
+        pass
+
+    def creation(self):
+        pass
+
+    def deletion(self):
+        pass
+
+    def resolution(self):
+        pass
+
