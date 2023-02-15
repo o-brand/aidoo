a7fc7bac01b7b3a1af4b7eeb56aa5566152490e4
Roman Berger <romanberger001@gmail.com>
2023-02-15 09:00:03 +0000

    (starting) tests for the report model

--- Golf/superadmin/tests.py
+++ Golf/superadmin/tests.py
@@ -1,16 +1,28 @@
 from django.test import TestCase
+from Golf.utils import LoginRequiredTestCase
+from django.db import models
+from jobs.models import Job
 
-class TestCaseReporting(TestCase):
+class TestCaseReporting(LoginRequiredTestCase):
+    """ tests for the reports model """
 
     def setUp(self):
+        # setup these things
+        # users
+        # jobs
+        # require being logged in
+
+        super().setUp()
         pass
 
-    def creation(self):
+    def insertion(self):
+        # insert a report
         pass
 
     def deletion(self):
+        # delete a report
         pass
 
     def resolution(self):
+        # change the status of the report
         pass
-
