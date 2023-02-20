f982101b1cd0db56defce8ca8dd318cb7fe72e4f
Rosta Gábor <rostagabor13@gmail.com>
2023-02-20 10:36:11 +0000

    Heroku changes

--- Golf/Golf/asgi.py
+++ Golf/Golf/asgi.py
@@ -8,14 +8,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Golf.settings")
 
 asgi_app = get_asgi_application()
+
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": asgi_app,
