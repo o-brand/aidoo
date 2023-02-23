35fd78b8631c527f98e792bd7b0d526421318d5b
Kieronr142 <u09kr18@abdn.ac.uk>
2023-02-23 16:36:12 +0000

    job posting modal hour to hours
    js code to update the word hour to hours when the selected hours is plural

--- Golf/static/js/postjob.js
+++ Golf/static/js/postjob.js
@@ -2,6 +2,7 @@
 var points = 12;
 var hhours = 0;
 var hours = 1;
+var hourstxt = 'hour';
 
 // Recalculate points
 $("select[name='duration_hours']").on("input", function(e) {
@@ -12,7 +13,13 @@ $("select[name='duration_hours']").on("input", function(e) {
     points += (value - hours) * 12;
     hours = value;
 
+    if (points > 18)
+      hourstxt = 'hours'
+    else
+      hourstxt = 'hour'
+
     //Display the new points
+    $("#hourstxt").html(hourstxt)
     $("#points").html(points);
   }
 });
@@ -24,7 +31,13 @@ $("select[name='duration_half_hours']").on("input", function(e) {
     points += (value - hhours) / 60 *12;
     hhours = value;
 
+    if (points > 18)
+      hourstxt = 'hours'
+    else
+      hourstxt = 'hour'
+
     //Display the new points
+    $("#hourstxt").html(hourstxt)
     $("#points").html(points);
   }
 });
