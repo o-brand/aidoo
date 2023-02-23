0f27833eb646875382b1255ff644e7c74d3648ee
anet.k <u06ak20@abdn.ac.uk>
2023-02-23 15:11:23 +0000

    duration fields changed, migrations

--- Golf/static/js/postjob.js
+++ Golf/static/js/postjob.js
@@ -1,28 +1,28 @@
 //Points
-var points = 5;
-var days = 0;
+var points = 12;
+var hhours = 0;
 var hours = 1;
 
 // Recalculate points
-$("select[name='duration_days']").on("input", function(e) {
-  value = Number.parseFloat(e.target.value);
-
-  //If the new value is a normal number
-  if (!Number.isNaN(value)) {
-    points += (value - days) * 24 * 5;
-    days = value;
-
-    //Display the new points
-    $("#points").html(points);
-  }
-});
 $("select[name='duration_hours']").on("input", function(e) {
   value = Number.parseFloat(e.target.value);
 
   //If the new value is a normal number
   if (!Number.isNaN(value)) {
-    points += (value - hours) * 5;
+    points += (value - hours) * 12;
     hours = value;
+
+    //Display the new points
+    $("#points").html(points);
+  }
+});
+$("select[name='duration_half_hours']").on("input", function(e) {
+  value = Number.parseFloat(e.target.value);
+
+  //If the new value is a normal number
+  if (!Number.isNaN(value)) {
+    points += (value - hhours) / 60 *12;
+    hhours = value;
 
     //Display the new points
     $("#points").html(points);
