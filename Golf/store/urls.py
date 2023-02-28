c17024461c24292e8fe2403598aa76c858d6ea0e
Gábor Rosta <32847735+gaborrosta@users.noreply.github.com>
2023-02-28 15:26:45 +0000

    Small fix

--- Golf/store/urls.py
+++ Golf/store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from store.views import generate_QRcode
+from store.views import send_QRcode
 from django.urls import re_path
 
 
@@ -10,7 +10,6 @@ urlpatterns = [
     path("", views.home, name="store"),
     # Buy an item from the store, uses HTMX
     path("buyitem", views.buyitem_call, name="buyitem"),
-    re_path(r'^qrcode/(?P<id>.+)$', generate_QRcode, name='qrcode'),
+    # ???
+    re_path(r'^qrcode/(?P<id>.+)$', send_QRcode, name='qrcode'),
 ]
-
- 
