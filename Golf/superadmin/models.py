0b827a7b20e1d66fcb99c71af80b2ad0252fba09
Rosta Gábor <rostagabor13@gmail.com>
2023-03-06 22:35:54 +0000

    Deleted unnecessary primary key for report

--- Golf/superadmin/models.py
+++ Golf/superadmin/models.py
@@ -24,10 +24,7 @@ class Report(models.Model):
         OPEN = 'Open', ('Open')
         TICKETED = 'Ticketed', ('Ticketed')
         RESOLVED = 'Resolved', ('Resolved')
-    
-    # Primary key
-    report_id = models.BigAutoField(primary_key=True)
-    
+
     # ID of reported job, if the report concerns a job post
     reported_job = models.ForeignKey(Job,
         on_delete=models.CASCADE,
