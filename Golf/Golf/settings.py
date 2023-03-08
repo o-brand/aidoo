e101e6b007f5d23eec73817f428a5ac107b481a2
Rosta Gábor <rostagabor13@gmail.com>
2023-03-08 10:18:50 +0000

    Updated email

--- Golf/Golf/settings.py
+++ Golf/Golf/settings.py
@@ -175,8 +175,8 @@ LOGOUT_REDIRECT_URL = "/"  # After log out, the user is redirected to here.
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "smtpmailer.send@gmail.com"
-EMAIL_HOST_PASSWORD = "cjuvqbntoobtxwlc"
+EMAIL_HOST_USER = "aidoo.scot@gmail.com"
+EMAIL_HOST_PASSWORD = "phybzebqpwogrbid"
 EMAIL_USE_TLS = True
 
 
