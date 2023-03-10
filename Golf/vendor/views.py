83c12342bfaaafccfa9ac42699af723b91cca206
o-brand <o.brand.20@abdn.ac.uk>
2023-03-10 12:14:00 +0000

    More validation and styling

--- Golf/vendor/views.py
+++ Golf/vendor/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from cryptography.fernet import Fernet
 import base64
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.http import Http404
 from store.models import Sale
+
+
+# Get actual user model.
+User = get_user_model()
 
 
 def redeem(request, token):
@@ -14,15 +19,21 @@ def redeem(request, token):
         cipher_suite = Fernet(settings.KEY)
         fact = cipher_suite.decrypt(fact).decode("ascii")
 
-        sale, _ = fact.split("#")
-        item = Sale.objects.get(pk=sale).purchase
-    except Exception:
+        sale, buyer = fact.split("#")
+        sale = Sale.objects.get(pk=sale)
+        buyer = User.objects.get(pk=buyer)
+
+        if sale.buyer != buyer:
+            raise Http404()
+
+        item = sale.purchase
+
+    except Exception: # Should we use a more specific kind of exception?
         raise Http404()
         
     context = {
         "vendor" :"true",
         "item": item,
-        
     }
     return render(request, "vendor/index.html", context)
 
