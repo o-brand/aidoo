42158358882e250acabdac51a783e0c0d1a31f87
Rosta Gábor <rostagabor13@gmail.com>
2023-03-10 12:39:46 +0000

    Fixed monthly to be able to run monthly

--- Golf/monthly.py
+++ Golf/monthly.py
@@ -1,33 +1,38 @@
 # python monthly.py - DO NOT RUN LOCALLY!!
 
-import django
+import datetime
 
-django.setup()
+# If it is the first day of the month
+if datetime.datetime.today().day == 1:
 
-from django.contrib.auth import get_user_model
-from django.contrib.sites.models import Site
-from store.models import Moderation
+    import django
+
+    django.setup()
+
+    from django.contrib.auth import get_user_model
+    from django.contrib.sites.models import Site
+    from store.models import Moderation
 
 
-# Get actual user model.
-User = get_user_model()
+    # Get actual user model.
+    User = get_user_model()
 
 
-# Get charities
-charities = User.objects.filter(charity=True)
-number_of_charities = len(charities)
+    # Get charities
+    charities = User.objects.filter(charity=True)
+    number_of_charities = len(charities)
 
-# Get site with the bank's balance
-site = Site.objects.get_current().moderation
+    # Get site with the bank's balance
+    site = Site.objects.get_current().moderation
 
-# Calculate the donation
-donation = int(site.bank / number_of_charities)
+    # Calculate the donation
+    donation = int(site.bank / number_of_charities)
 
-# Give the doos to the charities
-for charity in charities:
-    charity.balance += donation
-    charity.save()
-    site.bank -= donation
+    # Give the doos to the charities
+    for charity in charities:
+        charity.balance += donation
+        charity.save()
+        site.bank -= donation
 
-# Save new bank balance
-site.save()
+    # Save new bank balance
+    site.save()
