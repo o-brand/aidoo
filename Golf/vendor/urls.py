8db71e0c352e69cfe40ad19fc20878666712561d
Rosta Gábor <rostagabor13@gmail.com>
2023-03-11 16:08:39 +0000

    Redeeming done

--- Golf/vendor/urls.py
+++ Golf/vendor/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 # Be aware that the url already includes "vendor/".
 urlpatterns = [
-    # 
+    # Redeeming a bought item, used by HTMX
+    path("redeem", views.redeem_call, name="redeem-item"),
+    # Used to display a bought item to redeem 
     path("<token>", views.redeem, name="redeem"),
 ]
--- Golf/vendor/views.py
+++ Golf/vendor/views.py
@@ -12,7 +12,7 @@ User = get_user_model()
 
 
 def redeem(request, token):
-    # Render the page
+    """Displays a bought item to be redeemed."""
 
     try:
         fact = base64.urlsafe_b64decode(token)
@@ -28,14 +28,52 @@ def redeem(request, token):
 
         item = sale.purchase
 
-    except Exception: # Should we use a more specific kind of exception?
+    except Exception:
         raise Http404()
-        
+
     context = {
         "vendor" :"true",
+        "redeemed": sale.redeemed,
         "item": item,
+        "sale": sale.pk,
+        "buyer": buyer.pk,
     }
     return render(request, "vendor/index.html", context)
 
+
 def redeem_call(request):
-    pass
+    """Redeem a bought item."""
+    if request.method == "POST":
+        # Get the sale ID or -1 if it is not found
+        sale_id = request.POST.get("sale", -1)
+
+        # Get the user ID or -1 if it is not found
+        buyer_id = request.POST.get("buyer", -1)
+
+        # Check if the sale ID is valid
+        sales = Sale.objects.filter(sale_id=sale_id)
+        sale_id_exists = len(sales) == 1
+        if not sale_id_exists:
+            raise Http404()
+        sale = sales[0]
+
+        # Check if the user ID is valid
+        buyers = User.objects.filter(id=buyer_id)
+        buyer_id_exists = len(buyers) == 1
+        if not buyer_id_exists:
+            raise Http404()
+        buyer = buyers[0]
+
+        # Check if the buyer is the actual buyer
+        if sale.buyer != buyer:
+            raise Http404()
+
+        # Redeem the item
+        # If the item was redeemed before, then nothing happens now.
+        sale.redeemed = True
+        sale.save()
+
+        return render(request, "vendor/redeemed.html")
+
+    # If it is not POST
+    raise Http404()
