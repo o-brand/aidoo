df1769b1560c4e75d0cbe39c39d25b9275e05bde
Rosta Gábor <rostagabor13@gmail.com>
2023-03-12 16:24:38 +0000

    Fixed buying process

--- Golf/static/js/store.js
+++
@@ -1,6 +0,0 @@
-$(".form-buy").each(function() {
-    $(this).on("click", function (event) {
-      console.log("Something happens")
-      location.reload()
-    });
-});
--- Golf/store/views.py
+++ Golf/store/views.py
@@ -23,6 +23,26 @@ from .models import Item, Sale, Transfer
 User = get_user_model()
 
 
+def calculate_choices(me, item, purchases):
+    # Limit per user - already bought OR the stock
+    if item.limit_per_user is not None:
+        can_buy_below_limit = item.limit_per_user - sum(
+            [x.quantity for x in purchases]
+        )
+    else:
+        can_buy_below_limit = item.stock
+
+    # The maximal quantity that can be bought using the balance
+    can_afford = me.balance // item.price
+
+    # Takes the minimum of the (limit per user - already bought OR the stock),
+    # the stock, the display limit of 5, and the maximal quantity that can be bought using the balance
+    return range(
+        1,
+        min(can_buy_below_limit, item.stock, 5, can_afford) + 1,
+    )
+
+
 def home(request):
     """Render the main shop page"""
     me = request.user
@@ -32,23 +52,8 @@ def home(request):
 
     forms = dict()
     for item in items:
-        # Limit per user - already bought OR the stock
-        if item.limit_per_user is not None:
-            can_buy_below_limit = item.limit_per_user - sum(
-                [x.quantity for x in purchases.filter(purchase=item)]
-            )
-        else:
-            can_buy_below_limit = item.stock
-        
-        # The maximal quantity that can be bought using the balance
-        can_afford = me.balance // item.price
-
-        # Takes the minimum of the (limit per user - already bought OR the stock),
-        # the stock, the display limit of 5, and the maximal quantity that can be bought using the balance
-        values = range(
-            1,
-            min(can_buy_below_limit, item.stock, 5, can_afford) + 1,
-        )
+        # Calculate the available choices
+        values = calculate_choices(me, item, purchases.filter(purchase=item))
 
         if len(values) > 0:
             forms[item] = BuyForm(values)
@@ -64,46 +69,73 @@ def home(request):
 
 
 def buyitem_call(request):
+    """Buy an item and return back the new form."""
     if request.method == 'POST':
-        me = request.user
-        buyer = me
+        buyer = request.user
 
-        try:
-            item_id = int(request.POST.get("purchase", -1))
-        except ValueError:
-            raise Http404()
+        # Get the item ID or -1 if it is not found
+        item_id = request.POST.get("purchase", -1)
 
+        # Check if the item ID is valid
         items = Item.objects.filter(pk=item_id)
-        if item_id < 0 or len(items) != 1:
+        item_id_exists = len(items) == 1
+        if not item_id_exists:
             raise Http404()
         item = items[0]
 
+        # Check if the item is on offer
         if not item.on_offer:
             raise Http404()
 
-        choices = range(1, min(
-            item.limit_per_user - sum([x.quantity for x in Sale.objects.filter(
-                purchase=item, buyer=me)])
-            if item.limit_per_user is not None else item.stock,
-            me.balance//item.price, item.stock, 5)+1)
-
+        # Recreate the form
+        purchases = Sale.objects.filter(buyer=buyer, purchase=item)
+        choices = calculate_choices(buyer, item, purchases)
         form = BuyForm(choices, data=request.POST)
+
+        # Check the quantity and available fund
         try:
             quantity = int(form.data["quantity"])
             if buyer.balance < quantity * item.price:
-                raise Http404()
+                # Recreate the form (so the user cannot buy it again)
+                purchases = Sale.objects.filter(buyer=buyer, purchase=item)
+                choices = calculate_choices(buyer, item, purchases)
+
+                if len(choices) > 0:
+                    form = BuyForm(choices)
+                else:
+                    form = False
+                
+                # Return
+                response = render(
+                    request, "store/buy-form.html", {"form": form, "item": item}
+                )
+                response['HX-Trigger'] = "rebalance"
+                return response
         except ValidationError:
             raise Http404()
-        if form.is_valid():
-            data = []
-            site = get_current_site(request)
-            for _ in range(quantity):
-                sale = Sale.objects.create(
-                purchase = item,
-                buyer = buyer,
-                quantity = 1)
 
+        # If everything is valid
+        if form.is_valid():
+            # Store the data for the QR codes
+            data = []
+
+            # Get the current site (for domain, bank)
+            site = get_current_site(request)
+
+            # Create Sale objects...
+            for _ in range(quantity):
+                # A new sale object with quantity 1
+                sale = Sale.objects.create(
+                    purchase = item,
+                    buyer = buyer,
+                    quantity = 1
+                )
+
+                # Save it
                 sale.save()
+
+                # Generate a token for the QR code based on
+                # the sale id and the buyer id
                 try:
                     fact = f"{sale.pk}#{buyer.pk}"
                     cipher_suite = Fernet(settings.KEY)
@@ -113,20 +145,24 @@ def buyitem_call(request):
                 except Exception:
                     raise Http404()
 
-
+            # Decrease the balance of the buyer and increase the bank's balance
             buyer.balance = buyer.balance - item.price * quantity
             site.moderation.bank += item.price * quantity
-            # Reduce the stock of the item by the sale qty
+
+            # Reduce the stock of the item by the sale quantity
             item.stock = item.stock - quantity
 
-            send_QRcode(buyer.email, data)
-
+            # Save everything
             item.save()
             buyer.save()
             site.moderation.save()
 
+            # Send the email with the QR codes
+            send_QRcode(buyer.email, data)
+
+            # Create a notification
             notification = Notification.objects.create(
-                user_id=me,
+                user_id=buyer,
                 title=f"New purchase",
                 content=(
                     (f"Thank you for buying {item.item_name}."
@@ -136,10 +172,23 @@ def buyitem_call(request):
             )
             notification.save()
 
-            return HttpResponse(
-                status=204,
-                headers={"HX-Trigger": "rebalance"})
+            # Recreate the form (so the user can buy it again)
+            purchases = Sale.objects.filter(buyer=buyer, purchase=item)
+            choices = calculate_choices(buyer, item, purchases)
 
+            if len(choices) > 0:
+                form = BuyForm(choices)
+            else:
+                form = False
+            
+            # Return
+            response = render(
+                request, "store/buy-form.html", {"form": form, "item": item}
+            )
+            response['HX-Trigger'] = "rebalance"
+            return response
+
+    # If it is not POST
     raise Http404()
 
 
